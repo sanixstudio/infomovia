ae64a8247d5b8aaeba962e33effb45d6dc76ea95
Adnan Niaz <adnanniaz77@yahoo.com>
2021-03-13 23:31:42 -0800

    made dom reference to js and got api setup

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+const API_KEY = "1dc3a1bc9c9d1a12ed9931344d82ebc1";
+const URL_BASE = 'https://api.themoviedb.org/3/';
+const URL_IMG = "https://image.tmdb.org/t/p/w185/";
+const URL_UPCOMING = `${URL_BASE}movie/upcoming?language=en-US&api_key=${API_KEY}`;
+const URL_PLAYING_NOW = `${URL_BASE}movie/now_playing?language=en-US&api_key=${API_KEY}`;
+const URL_TOP_RATED = `${URL_BASE}movie/top_rated?language=en-US&api_key=${API_KEY}`;
+const URL_SEARCH = `${URL_BASE}search/movie?api_key=${API_KEY}&language=en-US&query=${"movieName"}&page=1&include_adult=false`;
+
+const getMoviesCategory = async (category, selector) => {
+    const res = await (await fetch(category)).json();
+
+    for (let movie of res.results) {
+        console.log(movie);
+        const col = document.querySelector(selector);
+        let temp = `<div><img class="poster" src=${URL_IMG + movie.poster_path || "null"} alt=""></div>`
+    }
+
+}
+
+getMoviesCategory(URL_PLAYING_NOW, ".playing_now")
+getMoviesCategory(URL_UPCOMING, '.upcoming')
+getMoviesCategory(URL_TOP_RATED, '.top_rated')
