9fd81086f0c921dbb328d7e88bbdefaf2dbcbf2f
Adnan Niaz <adnanniaz77@yahoo.com>
2021-03-18 09:39:24 -0700

    saving

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,11 @@
+let searchQuery = "";
 const API_KEY = "1dc3a1bc9c9d1a12ed9931344d82ebc1";
 const URL_BASE = 'https://api.themoviedb.org/3/';
 const URL_IMG = "https://image.tmdb.org/t/p/w185/";
 const URL_UPCOMING = `${URL_BASE}movie/upcoming?language=en-US&api_key=${API_KEY}`;
 const URL_PLAYING_NOW = `${URL_BASE}movie/now_playing?language=en-US&api_key=${API_KEY}`;
 const URL_TOP_RATED = `${URL_BASE}movie/top_rated?language=en-US&api_key=${API_KEY}`;
-const URL_SEARCH = `${URL_BASE}search/movie?api_key=${API_KEY}&language=en-US&query=${"movieName"}&page=1&include_adult=false`;
+const URL_SEARCH = `${URL_BASE}search/movie?api_key=${API_KEY}&language=en-US&query=${searchQuery}&page=1&include_adult=false`;
 
 /************************************************************
     Get All category (Playing Now, Upcoming and Top-rated)
@@ -15,12 +16,34 @@ const getMoviesCategory = async (category, selector) => {
 
     for (let movie of res.results) {
         const className = document.querySelector(selector);
-        let temp = `<div><img class="poster" src=${URL_IMG + movie.poster_path || "null"} alt=""></div>`;
+        let temp = `<div><img class="poster" src=${URL_IMG + movie.poster_path || ""} alt=""></div>`;
         className.innerHTML += temp;
     }
 
 }
 
+// Display the movies to dom
 getMoviesCategory(URL_PLAYING_NOW, ".playing_now")
 getMoviesCategory(URL_UPCOMING, '.upcoming')
 getMoviesCategory(URL_TOP_RATED, '.top_rated')
+
+/************************************************************
+                Search Movies Information
+*************************************************************/
+
+const searchForm = document.querySelector('#theForm');
+const keywords = document.querySelector('#keywords');
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let cleanQuery = "";
+    keywords.value !== "" ?  cleanQuery = keywords.value : alert('Please enter your keyword')
+    searchQuery = cleanQuery.trim();
+    console.log(searchQuery)
+})
+
+const search = async (query) => {
+    query && console.log(query)
+}
+
+search(searchQuery);
